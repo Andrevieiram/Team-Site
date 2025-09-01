3e64d127344cf145b08e891c712c709ac0206836
Samuel Soares <samuelsoares.academic@gmail.com>
2025-09-01 16:17:46 -0300

    Subindo programação feita por Alexandre (Teve problemas com o git)

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById("contactForm");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("Mensagem enviada com sucesso!");
-  form.reset();
-});
